bd7949f1bd794d085aaa655e1654a66bab00b870
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 20:33:43 -0300

    Aula 003 de JS

---
+++ aprendendo-js/primeiros-passos/aula003/script.js
@@ -0,0 +1,16 @@
+var user
+console.log(user)
+
+
+var email = "kaarlosvinicius2703@gmail.com"
+console.log(email)
+
+
+email = "karlos_vinicius27@hotmail.com"
+console.log(email)
+
+var product = "Teclado Mecânico"
+var Product = "Teclado Normal"
+
+console.log(product)
+console.log(Product)
