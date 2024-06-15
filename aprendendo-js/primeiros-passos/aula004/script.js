7e89d3260f6614d3ffaaf80d7398b3af967e9438
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 20:36:45 -0300

    Aula 004 de JS

---
+++ aprendendo-js/primeiros-passos/aula004/script.js
@@ -0,0 +1,11 @@
+let user
+console.log(user)
+
+user = "Karlos Vinícius"
+console.log(user) 
+
+let email = "kaarlosvinicius2703@gmail.com"
+console.log(email)
+
+email = "karlos_vinicius27@hotmail.com"
+console.log(email)
