b23514aed1d1d90994b21d6c93dd36673a01f9aa
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:20:28 -0300

    Aula 016 de JS - Operadores aritmético

---
+++ aprendendo-js/primeiros-passos/aula016/script.js
@@ -0,0 +1,20 @@
+// Operadores
+
+// Expressão - Combinação de valores, variáveis e operadores
+
+let sum = 5 + 2 + 7;
+
+let average = sum / 3;
+
+// Operadores aritméticos
+console.log("SOMA: ", 12 + 8);
+
+console.log("SUBTRAÇÃO: ", 12 - 8); 
+
+console.log("MULTIPLICAÇÃO: ", 3 * 5.5);
+
+console.log("DIVISÃO: ", 12 / 3);
+
+console.log("MÓDULO: ", 12 % 3); // Resto da divisão 
+
+console.log("EXPONENCIAL: ", 3 ** 3)
