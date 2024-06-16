dfdb42321f79da408453cf689f59cdc23b9e089b
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:49:45 -0300

    Aula 020 de JS - Menor e Maior

---
+++ aprendendo-js/primeiros-passos/aula020/script.js
@@ -0,0 +1,14 @@
+let balance = 500 
+let payment = 120 
+
+// > Maior que 
+console.log(balance > payment)
+
+// < Menor que 
+console.log(balance < payment)
+
+// >= Maior ou igual 
+console.log(balance >= payment)
+
+// <= Menor ou igual 
+console.log(balance <= payment)
