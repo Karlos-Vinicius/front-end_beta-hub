ea2d39f07bed21810fafe992da4ce19d2c48db2b
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 21:31:24 -0300

    Aula 009 de JS - Tipos de dados

---
+++ aprendendo-js/primeiros-passos/aula009/script.js
@@ -0,0 +1,11 @@
+// Tipos Primitivos - Disponíveis na própria linguagem
+
+/*
+
+    string - texto
+    number - inteiros, decimais...
+    boolean - Verdadeiro ou Falso 
+    null - 
+    undefined - Atribuído para uma variável que não armazena nada.
+
+*/
