8b6b1098ef7824c8acb99ed746ef07c3b51d430b
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:36:49 -0300

    Aula 019 de JS - Igual e Diferente.

---
+++ aprendendo-js/primeiros-passos/aula019/script.js
@@ -0,0 +1,17 @@
+let one = 1
+let two = 2
+
+
+// == igual a 
+console.log(one == two)
+console.log(one == 1)
+console.log(one == "1") // Olha para o conteúdo e não para o tipo
+
+console.log(one === "1") // Olha para o conteúdo e para o tipo
+
+// != diferente de 
+console.log(one != two) 
+console.log(one != 1) 
+console.log(one != "1")
+
+console.log(one !== "1")
