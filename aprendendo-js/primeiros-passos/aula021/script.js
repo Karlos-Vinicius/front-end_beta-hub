3f7077088a340c417dd24e8e92ef6f2f24ae78a8
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:52:22 -0300

    Aula 021 de JS - Operadores de atribuição

---
+++ aprendendo-js/primeiros-passos/aula021/script.js
@@ -0,0 +1,30 @@
+let value 
+
+// Operador de Atribuição 
+
+value = 1 
+console.log(value) 
+
+// Incremento 
+value += 2
+console.log(value)
+
+// Decremento 
+value -= 2
+console.log(value)
+
+// Multiplicação 
+value *= 2
+console.log(value)
+
+// Divisão 
+value /= 2
+console.log(value)
+
+// Módulo 
+value %= 2
+console.log(value)
+
+// Potência
+value **= 2
+console.log(value)
