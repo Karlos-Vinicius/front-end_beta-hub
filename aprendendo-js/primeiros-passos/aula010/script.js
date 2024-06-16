597c7c88b17782f39f27319199317851d3bbc237
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 21:36:59 -0300

    Aula010 de JS - Strings

---
+++ aprendendo-js/primeiros-passos/aula010/script.js
@@ -0,0 +1,13 @@
+// String 
+
+let username = "Karlos"
+console.log(username)
+console.log(typeof username)
+
+console.log("Uma string com aspas")
+console.log("Uma string com apóstrofo (aspas simples)")
+
+console.log(`
+    Uma string com acento grave permite 
+    escrever múltiplas linhas.    
+`)
