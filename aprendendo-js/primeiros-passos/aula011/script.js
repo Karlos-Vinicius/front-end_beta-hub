d5cd297ddd0747fefb457d22d103b04bf34d92d3
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 21:41:30 -0300

    Aula 011 de JS Template Strings

---
+++ aprendendo-js/primeiros-passos/aula011/script.js
@@ -0,0 +1,13 @@
+// Template literals (template strings, interpolaçõa de string) 
+
+let username = "Karlos"
+let email = "kaarlosvinicius2703@gmail.com"
+
+console.log(username, email, "teste")
+
+// Concatenar (somar texto)
+
+console.log("Olá, " + username + ". Você conectou com o e-mail " + email)
+
+// Template literals
+console.log(`Olá, ${username}. Você conectou com o e-mail ${email}`)
