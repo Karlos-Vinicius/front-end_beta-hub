8aa304080940d2a7e439a231f64a5bf46145a405
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 21:51:16 -0300

    Aula 014 de JS - Undefined e Null

---
+++ aprendendo-js/primeiros-passos/aula014/script.js
@@ -0,0 +1,7 @@
+// Undefined (indefinido) e null
+
+let emptiness
+console.log("O valor é: ", emptiness)
+
+let empty = null // Usamos o null quando dizemos (intensionalmente) que a variável está vazia.
+
