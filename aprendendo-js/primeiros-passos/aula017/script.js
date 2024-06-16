03018b6507ad6c09f86ae24773878ccfe8cb228d
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:25:18 -0300

    Aula 017 de JS - Incremento e Decremento.

---
+++ aprendendo-js/primeiros-passos/aula017/script.js
@@ -0,0 +1,13 @@
+let number = 10 
+
+number++ // Operador de Incremento - number = number + 1
+console.log(number)
+
+number-- // Operador de Decremento - number = number - 1
+
+console.log(number)
+console.log(number++) // Imprime no terminal e depois soma
+console.log(++number) // Soma e depois imprime no terminal
+
+number += 10 // number = number + 10 
+number -= 10 // number = number - 10 
