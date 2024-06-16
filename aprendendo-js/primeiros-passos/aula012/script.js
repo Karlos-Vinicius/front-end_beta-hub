12baacde00cc6903d842cc4c8ae7148199ec2e49
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 21:44:07 -0300

    Aula 012 de JS - Number

---
+++ aprendendo-js/primeiros-passos/aula012/script.js
@@ -0,0 +1,15 @@
+// Number
+
+console.log(typeof 5) 
+
+// Inteiro positivo 
+console.log(5) 
+
+// Inteiro negativo
+console.log(-5)
+
+// Números reais ou float
+console.log(5.55)
+
+// NaN - Not a Number
+console.log(12.5 / "Karlos")
