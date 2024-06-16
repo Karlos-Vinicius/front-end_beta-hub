90f78994271c520c0008d3ce90b0495e947cf513
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:55:49 -0300

    Aula 022 de JS - Operadores lógicos

---
+++ aprendendo-js/primeiros-passos/aula022/script.js
@@ -0,0 +1,11 @@
+let email = true
+let password = true
+
+// AND (E) && 
+console.log(email && password)
+
+// OR (OU) ||
+console.log(email || password)
+
+// NOT (NÃO) !
+console.log(!password) // Inverte a lógica
