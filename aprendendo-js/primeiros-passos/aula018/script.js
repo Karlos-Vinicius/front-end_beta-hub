0df40e03d54b77e11e389d524dfec498cf1e50da
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2024-06-15 22:33:54 -0300

    Aula 018 de JS - Quebrando a ordem de precedência.

---
+++ aprendendo-js/primeiros-passos/aula018/script.js
@@ -0,0 +1,11 @@
+// Ordem de Precedência - Define qual operação vai ser realizada primeiro 
+// O parênteses altera a ordem de precedência.
+
+let total = 2 + 3 * 4 
+console.log(total)
+
+let total2 = (2 + 3) * 4 
+console.log(total2)
+
+let average = (9.5 + 8.3) / 2
+console.log(average)
